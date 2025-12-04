1561e1fcc4727a596945df9e84c24825e42cbecd
Mimonsi <konstantin-schwarze@outlook.de>
2025-12-04 10:49:41 +0100

    Release final version.

--- Mod.cs
+++ Mod.cs
@@ -1,60 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Game;
 using Game.Modding;
-using Game.SceneFlow;
-using Colossal.IO.AssetDatabase;
 using Colossal.Logging;
 using Colossal.PSI.Environment;
-using Colossal.UI;
-using Game.Prefabs;
-using Game.PSI;
-using JetBrains.Annotations;
-using UnityEngine;
-using Hash128 = Colossal.Hash128;
-using StreamReader = System.IO.StreamReader;
 
 namespace AssetPacksManager
 {
     public class Mod : IMod
     {
-        public string ModPath { get; set; }
-
-        public static ApmLogger Logger = new();
+        public static ILog log = LogManager.GetLogger("AssetPacksManager");
 
         public void OnLoad(UpdateSystem updateSystem)
         {
-            ApmLogger.Init();
-
-            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+            try
             {
-                //if (!DisableLogging)
-                //    Logger.Info($"Current mod asset at {asset.path}");
-                ModPath = Path.GetDirectoryName(asset.path);
-                UIManager.defaultUISystem.AddHostLocation("apm", Path.Combine(Path.GetDirectoryName(asset.path), "Resources"), false);
+                var modsDataPath = Path.Combine(EnvPath.kUserDataPath, "ModsData", "AssetPacksManager");
+                if (Directory.Exists(modsDataPath))
+                {
+                    log.Info("Loading Final Version of Asset Packs Manager Mod");
+                    Directory.Delete(modsDataPath, true);
+                    log.Info("Deleted Asset Packs Manager ModsData to prevent conflicts. It will no longer be used. Thank you everyone, it's been an honor. Asset Packs Manager signing off.");
+                }
             }
-
-            Setting setting = new (this);
-            setting.RegisterInOptionsUI();
-            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(setting));
-            AssetDatabase.global.LoadSettings(nameof(AssetPacksManager), setting, new Setting(this));
-            Setting.Instance = setting;
-            Setting.Instance.UpdateLogLevel();
-            Logger.Info(Setting.Instance.ToString());
-
-            updateSystem.UpdateAt<AssetPackLoaderSystem>(SystemUpdatePhase.MainLoop);
+            catch (Exception e)
+            {
+                log.Warn("Error while trying to delete Asset Packs Manager ModsData folder: " + e);
+            }
         }
 
         public void OnDispose()
         {
-            if (Setting.Instance != null)
-            {
-                Setting.Instance.UnregisterInOptionsUI();
-                Setting.Instance = null;
-            }
+
         }
 
     }
